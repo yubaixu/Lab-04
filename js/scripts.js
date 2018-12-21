59dd01889ea75f8ff4998b4110a2835bfe4783d8
Brian Andere <33161690+briancollins081@users.noreply.github.com>
2018-12-21 18:33:43 +0300

    Exercise 2: using JavaScript to add a class DONE

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log("hello");
+window.onload=function(){
+    const menuToggle = document.getElementById("menu-toggle");
+    const menuNav = document.getElementById("menu-nav");
+    function toggleMenu(){
+        console.log("called toggleMenu");
+        menuNav.classList.toggle("menu-toggle");
+    }
+
+    menuToggle.addEventListener("click",toggleMenu);
+}
